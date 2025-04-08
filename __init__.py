9c7ec3fc2b3c47fb68066db5ebbe861b88d7451e
Tensor <git@tinkerer.eu>
2025-04-08 23:00:12 +0200

    Incremented version counter for new release.

--- __init__.py
+++ __init__.py
@@ -25,7 +25,7 @@
 bl_info = {
     "name": "Sansar Vertex Animation",
     "author": "Tensor",
-    "version": (1, 5),
+    "version": (1, 6),
     "blender": (4, 2, 0),
     "location": "View3D > Sidebar > Sansar Tools Tab",
     "description": "A tool for storing per frame vertex data for use in Sansar VAT shader.",
